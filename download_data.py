a52e90ce674c62b1ee973588cacbe54e5a50e85f
Đào Huy Vũ <vudh@inergy.edu.vn>
2025-03-09 05:58:42 +0700

    Modify download file

--- download_data.py
+++ download_data.py
@@ -5,4 +5,4 @@ import shutil
 
 
 #os.system("git clone https://github.com/gveres/donateacry-corpus.git")
-shutil.copytree('donateacry-corpus\\donateacry_corpus_cleaned_and_updated_data\\', 'dataset') # target filename is /dst/dir/file.ext
+shutil.copytree('donateacry-corpus/donateacry_corpus_cleaned_and_updated_data/', 'dataset') # target filename is /dst/dir/file.ext
